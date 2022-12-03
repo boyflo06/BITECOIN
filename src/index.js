128d0deb6d516a61d5f9b50b0ebf6b5a43fe38d8
boyflo06 <71519064+boyflo06@users.noreply.github.com>
2022-12-03 23:33:56 +0100

    made the sign-up work, tweaked some css and added a page for the user info

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
 import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js"
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -21,15 +23,109 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth();
 const db = getDatabase(app);
-function writeUserData(userId, name, email, imageUrl) {
+var name = document.getElementById("name-input").value
+var email = document.getElementById('email-input').value
+var password = document.getElementById('pswrd-input').value
+var password2 = document.getElementById("pswrd-confirm-input").value
+
+function register () {
+  // Get all our input fields
+  name = document.getElementById("name-input").value
+  email = document.getElementById('email-input').value
+  password = document.getElementById('pswrd-input').value
+  password2 = document.getElementById("pswrd-confirm-input").value
+
+  // Validate input fields
+  if (validate_email(email) == false) {
+    document.getElementById("email-error").innerHTML = 'Email is Outta Line!!'
+    return
+    // Don't continue running the code
+  } else {
+    document.getElementById("email-error").innerHTML = ''
+  }
+  if (validate_password(password, password2) == false) {
+    document.getElementById("pswrd-error").innerHTML = "Password is too short or doesnt match"
+    return
+  } else {
+    document.getElementById("pswrd-error").innerHTML = ''
+  }
+  if (validate_field(name) == false || validate_field(email) == false || validate_field(password) == false || validate_field(password2) == false) {
+    document.getElementById("value-error").innerHTML = 'One or More Extra Fields is Outta Line!!'
+    return
+  } else {
+    document.getElementById("value-error").innerHTML = ''
+  }
+ 
+  // Move on with Auth
+  createUserWithEmailAndPassword(auth, email, password)
+  .then(function(userCredentials) {
+    // Declare user variable
+    var user = auth.currentUser
+    var uid = user.uid
+
+    // Add this user to Firebase Database
+    writeUserData(uid, name, email)
+
+    // DOne
+    window.location.href = ""
+  })
+  .catch(function(error) {
+    // Firebase will use this to alert of its errors
+    var error_code = error.code
+    var error_message = error.message
+
+    console.log(error_message)
+  })
+}
+
+
+//Validation functions
+function validate_email(email) {
+  var expression = /^[^@]+@\w+(\.\w+)+\w$/
+  if (expression.test(email) == true) {
+    // Email is good
+    return true
+  } else {
+    // Email is not good
+    return false
+  }
+}
+
+function validate_password(password, password2) {
+  // Firebase only accepts lengths greater than 6
+  if (password < 6) {
+    return false
+  } else if (password != password2) {
+    return false
+  } else {
+    return true
+  }
+}
+
+function validate_field(field) {
+  if (field == null) {
+    return false
+  }
+
+  if (field.length <= 0) {
+    return false
+  } else {
+    return true
+  }
+}
+
+function writeUserData(userId, name, email) {
   const db = getDatabase();
   set(ref(db, 'users/' + userId), {
-    username: name,
-    email: email,
-    password: pswrd,
-    role: role,
-    bank: money
+    email : email,
+    name : name,
+    bank : 0,
+    role : "membre",
+    verified : false,
+    last_login : Date.now()
   });
 }
 
+document.getElementById("signup-button").addEventListener("click", register)
