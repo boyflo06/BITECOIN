149d9d09345513c2c002b990c805837c9c1954d6
boyflo06 <71519064+boyflo06@users.noreply.github.com>
2022-12-03 14:36:31 +0100

    a lot

---
+++ src/index.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAxjhyOqE2og-V9TNvJ9NMaNxfxPtZUCas",
+  authDomain: "bitecoin-banking-app.firebaseapp.com",
+  databaseURL: "https://bitecoin-banking-app-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bitecoin-banking-app",
+  storageBucket: "bitecoin-banking-app.appspot.com",
+  messagingSenderId: "700081354570",
+  appId: "1:700081354570:web:dc24455628bff75782f3b3",
+  measurementId: "G-GJWGN4SDZZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getDatabase(app);
+function writeUserData(userId, name, email, imageUrl) {
+  const db = getDatabase();
+  set(ref(db, 'users/' + userId), {
+    username: name,
+    email: email,
+    password: pswrd,
+    role: role,
+    bank: money
+  });
+}
+
