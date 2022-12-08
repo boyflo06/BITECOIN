0ff9bf672060f37e62041a6cb187d962b1c46fc5
boyflo06 <boyflo06@gmail.com>
2022-12-08 17:32:35 +0100

    fixed js and added test for ios no adress bar

--- public/script/index.js
+++ public/script/index.js
@@ -20,22 +20,6 @@ const firebaseConfig = {
   measurementId: "G-GJWGN4SDZZ"
 };
 
-/* Get the documentElement (<html>) to display the page in fullscreen */
-var elem = document.documentElement;
-
-/* View in fullscreen */
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
-  }
-}
-
-openFullscreen();
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
